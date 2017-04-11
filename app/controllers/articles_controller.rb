8e2e6e1c286eb26dc43c3a27ebc52c79c51d74db
Colin Richardson <richardsonjcolin@gmail.com>
2017-04-11 18:23:39 +0900

    flash notices appear.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,11 +18,15 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
   	Article.destroy(params[:id])
+
+  	flash.notice = "Article exiled to the pits of article fire and brimstone!"
 
   	redirect_to articles_path
   end
@@ -34,6 +38,8 @@ class ArticlesController < ApplicationController
 	def update
   	@article = Article.find(params[:id])
   	@article.update(article_params)
+
+  	flash.notice = "Article '#{@article.title}' Updated!"
 
   	redirect_to article_path(@article)
 	end
